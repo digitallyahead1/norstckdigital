f5ec1df8aa2852abfbdee98d8a7e28411d47f1b9
USMAN BASHIR SANI <106992266+digitallyahead1@users.noreply.github.com>
2026-08-16 21:15:19 +0100

    feat: replace Capabilities & Solutions section with YouTube brand video showcase on Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Hero } from '../components/home/Hero';
 import { Marquee } from '../components/home/Marquee';
-import { BentoServices } from '../components/home/BentoServices';
+import { AdVideo } from '../components/home/AdVideo';
 import { ServicesCategory } from '../components/home/ServicesCategory';
 import { SystemSimulator } from '../components/home/SystemSimulator';
 import { Process } from '../components/home/Process';
 import { StrategyPlanner } from '../components/home/StrategyPlanner';
-import { AdVideo } from '../components/home/AdVideo';
 import { Testimonials } from '../components/home/Testimonials';
 import { FinalCTA } from '../components/home/FinalCTA';
 
@@ -23,8 +22,8 @@ export function Home({ onNavigate }: HomeProps) {
       {/* 2. Capability & Tech Marquee Ticker */}
       <Marquee />
 
-      {/* 3. Interactive Bento Services Carousel */}
-      <BentoServices onNavigate={onNavigate} />
+      {/* 3. Brand Video Showcase */}
+      <AdVideo />
 
       {/* 4. Services Category Explorer */}
       <ServicesCategory onNavigate={onNavigate} />
@@ -38,13 +37,10 @@ export function Home({ onNavigate }: HomeProps) {
       {/* 7. Interactive Strategy & Growth Configurator */}
       <StrategyPlanner onNavigate={onNavigate} />
 
-      {/* 8. Brand Video Showcase */}
-      <AdVideo />
-
-      {/* 9. Client Testimonials & Social Proof */}
+      {/* 8. Client Testimonials & Social Proof */}
       <Testimonials />
 
-      {/* 10. Finale Conversion CTA */}
+      {/* 9. Finale Conversion CTA */}
       <FinalCTA onNavigate={onNavigate} />
     </div>
   );
